d5b850b3b8de6855dec1eefc9ae0ec2c2e360df3
facundo brondo <facundobrondo34@gmail.com>
2022-06-21 19:54:22 -0300

    agregue JSON

--- js/main.js
+++ js/main.js
@@ -25,80 +25,124 @@ while ( userEmail != "" && confirmUserEmail != "")
 }
 
 
-class Curso { 
-    constructor(producto, precio, cantidad) {
-        this.producto = producto,
-        this.precio = precio,
-        this.cantidad = cantidad,
-        this.envio = 0,
-        this.subTotal = 0,
-        this.total = 0
-    }
-    calcularSubTotal() {
-        this.subTotal = this.precio * this.cantidad;
-    }
-    calcularEnvio() {
-        if (this.subTotal >=5000) {
-            this.envio = 0;
-        } else {
-            this.envio =700;
-        }
-    }
-    calcularIva() {
-        return this.subTotal * 0.21;
-    }
-    calcularTotal() {
-        this.total = this.subTotal + this.envio + this.calcularEnvio();
-    }
-}
+// class Curso { 
+//     constructor(producto, precio, cantidad) {
+//         this.producto = producto,
+//         this.precio = precio,
+//         this.cantidad = cantidad,
+//         this.envio = 0,
+//         this.subTotal = 0,
+//         this.total = 0
+//     }
+//     calcularSubTotal() {
+//         this.subTotal = this.precio * this.cantidad;
+//     }
+//     calcularEnvio() {
+//         if (this.subTotal >=5000) {
+//             this.envio = 0;
+//         } else {
+//             this.envio =700;
+//         }
+//     }
+//     calcularIva() {
+//         return this.subTotal * 0.21;
+//     }
+//     calcularTotal() {
+//         this.total = this.subTotal + this.envio + this.calcularEnvio();
+//     }
+// }
 
-const pedidoProducto = () => {
-    let producto = 0;
-    let cantidadProducto = 0;
-    let precio = 0;
+// const pedidoProducto = () => {
+//     let producto = 0;
+//     let cantidadProducto = 0;
+//     let precio = 0;
     
-    while (producto == 0 || producto > 4 || !producto) {
-        producto = parseInt(prompt("Que Curso deseas Comprar?:\n 1: Tecnica-de-extraccion($5000)\n 2:Taller-para-tostar($1800)\n 3:Receta-para-cafe($2000)"));
-    }
+//     while (producto == 0 || producto > 4 || !producto) {
+//         producto = parseInt(prompt("Que Curso deseas Comprar?:\n 1: Tecnica-de-extraccion($5000)\n 2:Taller-para-tostar($1800)\n 3:Receta-para-cafe($2000)"));
+//     }
 
-    switch(producto) {
-        case 1:
-            producto = "Tecnica-de-extraccion";
-            precio = 5000;
-            break;
-        case 2:
-            producto = "Taller-para-tostar";
-            precio = 1800;
-            break;
-        case 3:
-            producto = "Receta-para-cafe"; 
-            precio = 2000;
-            break;
+//     switch(producto) {
+//         case 1:
+//             producto = "Tecnica-de-extraccion";
+//             precio = 5000;
+//             break;
+//         case 2:
+//             producto = "Taller-para-tostar";
+//             precio = 1800;
+//             break;
+//         case 3:
+//             producto = "Receta-para-cafe"; 
+//             precio = 2000;
+//             break;
 
-    }
+//     }
 
-    while( cantidadProducto == 0 || !cantidadProducto ) {
-        cantidadProducto = parseInt(prompt("Producto elegido: "+ producto + "\n introduzca la cantidad deseada.(Solo numero)"));
-    }
+//     while( cantidadProducto == 0 || !cantidadProducto ) {
+//         cantidadProducto = parseInt(prompt("Producto elegido: "+ producto + "\n introduzca la cantidad deseada.(Solo numero)"));
+//     }
 
-    const Comprar = new pedido(producto, precio, cantidadProducto);
+//     const Comprar = new pedido(producto, precio, cantidadProducto);
 
-    return Comprar;
+//     return Comprar;
+// };
+
+
+// alert("Bienvenido/a la tienda online")
+
+// const pedido = pedidoProducto();
+
+// pedido.calcularTotal();
+// pedido.calcularEnvio();
+// pedido.calcularTotal();
+
+// alert("Detalle del pedido:\n\n"+
+//      "- "+pedido.Producto+ " x " +pedido.Cantidad+ ": $"+pedido.Precio * pedido.Cantidad +"\n" +
+//      "- IVA 21%:  $" +pedido.calcularIva()+ "\n" +
+//      "- Costo de envio: $"+pedido.Envio+ "\n\n" +
+//      "Total = $" +pedido.Total
+
+// );
+
+const carritoDeCursos = [
+    { id: 1, curso: "Tecnica de extraccion de cafe", precio: 2000 },
+    { id: 2, curso: "Taller para Tostar y Moler Granos", precio: 1000 },
+    { id: 3, curso: "4 Recetas de Café para Principiantes",  precio: 3500 }
+];
+
+const guardarLocal = (clave, valor) => {
+    localStorage.setItem(clave, valor);
 };
 
-alert("Bienvenido/a la tienda online")
 
-const pedido = pedidoProducto();
+guardarLocal("carrito", JSON.stringify(carritoDeCursos));
 
-pedido.calcularTotal();
-pedido.calcularEnvio();
-pedido.calcularTotal();
 
-alert("Detalle del pedido:\n\n"+
-     "- "+pedido.producto+ " x " +pedido.cantidad+ ": $"+pedido.precio * pedido.cantidad +"\n" +
-     "- IVA 21%:  $" +pedido.calcularIva()+ "\n" +
-     "- Costo de envio: $"+pedido.envio+ "\n\n" +
-     "Total = $" +pedido.total
+class Producto {
+    constructor(objeto) {
+        // this.nombre = objeto.nombre.toUpperCase();
+        this.precio = Number(objeto.precio);
+    }
 
-);
+    sumarIva() {
+        this.precio = this.precio * 1.21;
+    }
+};
+
+const almacenados = JSON.parse(localStorage.getItem("carrito"));
+
+
+const productos = [];
+
+for ( const objeto of almacenados ) {
+    productos.push( new Producto(objeto) )
+}
+
+
+for ( const producto of productos ) {
+    producto.sumarIva();
+}
+
+console.log(productos);
+
+
 
